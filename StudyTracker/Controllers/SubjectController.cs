b5cfc8ba8d7207c3a91349da5eaaeb199d603c5f
muaaz-abdool-gaffoor <muaazabdoolgaffoor@gmail.com>
2026-06-04 02:24:04 +0200

    Add subject and task models, subject controller, as well as subject creation view

---
+++ StudyTracker/Controllers/SubjectController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudyTracker.Models;
+
+namespace StudyTracker.Controllers
+{
+    public class SubjectController: Controller
+    {
+        
+        private readonly SubjectManager _subjectManager;
+        public SubjectController(SubjectManager subjectmanager)
+        {
+
+            _subjectManager = subjectmanager;
+
+        }
+
+
+
+        //This is our GET method, its job is just to show the form
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        //This is our POST method, its job is to take the data from the form and do something with it
+        [HttpPost] //this tells the program that this method is a POST method, and that form data should come here
+
+        public IActionResult Create (Subject subject)
+        {
+            _subjectManager.AddSubject(subject);
+            return RedirectToAction("index");
+
+        }
+
+
+    }
+}
--- StudyTracker/Program.cs
+++ StudyTracker/Program.cs
@@ -1,3 +1,5 @@
+using StudyTracker.Models;
+
 namespace StudyTracker
 {
     public class Program
@@ -8,6 +10,7 @@ namespace StudyTracker
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+            builder.Services.AddSingleton<SubjectManager>();
 
             var app = builder.Build();
 
